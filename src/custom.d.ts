2b34d3adfeaab93ef35659756fbb37c473acb9cc
chanystar <akwwkdrn@gmail.com>
2025-12-05 23:32:26 +0900

    vscode 에서 열때, 오류 수정

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -1,4 +1,24 @@
+declare module '*.css' {
+  const content: { [className: string]: string };
+  export default content;
+}
+
+declare module '*.scss' {
+  const content: { [className: string]: string };
+  export default content;
+}
+
 declare module '*.svg' {
-    const content: any;
-    export default content;
-  }
+  const content: any;
+  export default content;
+}
+
+declare module '*.png' {
+  const content: any;
+  export default content;
+}
+
+declare module '*.jpg' {
+  const content: any;
+  export default content;
+}
